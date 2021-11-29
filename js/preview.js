3e9abcedd11f364ffe4553bc369cc9f7efa8cbce
juljan94 <93835272+juljan94@users.noreply.github.com>
2021-11-29 22:29:46 +0300

    Update preview.js

--- js/preview.js
+++ js/preview.js
@@ -1,6 +1,6 @@
 var name = prompt("Si e ke emrin?");
 alert(`Ckemi, ${name}. Ke deshire te shikosh dicka te bukur?`);
 document.querySelector('html').innerHTML = `
-  <h1>Welcome to ${name}'s site!</h1>
+  <h1>Mire se erdhe ${name}!</h1>
   <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">
 `;
