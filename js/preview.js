d3f4e0d4c4616ef1445ea22fb6c78765012385b5
juljan94 <93835272+juljan94@users.noreply.github.com>
2021-12-06 14:03:05 +0300

    Delete js directory

--- js/preview.js
+++
@@ -1,6 +0,0 @@
-var name = prompt("Si e ke emrin?");
-alert(`Ckemi, ${name}. Ke deshire te shikosh dicka te bukur?`);
-document.querySelector('html').innerHTML = `
-  <h1>Mire se erdhe ${name}!</h1>
-  <img src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif">
-`;
